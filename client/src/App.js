4a387999f13966c175cc0afe4a75c85c3cf76444
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-10-13 17:41:23 +0200

    2

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,18 @@ import memories from './images/memories.png'
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 
+import useStyles from './styles'
+
 const App = () => {
+    const classes = useStyles();
+
     return (
        <Container maxidth="lg">
-           <AppBar position="static" color="inherit">
+           <AppBar className={classes.appBar} position="static" color="inherit">
                <Typography variant="h2" align="center">
                    Memories
                </Typography>
-               <img src={memories} alt="memories" height="100" />
+               <img className={classes.image} src={memories} alt="memories" height="60" />
            </AppBar>
            <Grow in>
                <Container>
