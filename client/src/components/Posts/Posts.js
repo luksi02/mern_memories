78b42bf610bc50af1b1a72febb6ed8445295bc74
Łukasz <103317892+luksi02@users.noreply.github.com>
2023-10-16 23:49:04 +0200

    5

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,6 +4,24 @@ import useStyles from "../../styles";
 
 const Posts = () => {
 
+  const dispatch = useDispatch();
+
+  const handleCreatePost = (postData) => {
+    dispatch(createPost(postData));
+  }
+
+  const handleUpdatePost = (postId, updatedPostData) => {
+    dispatch(updatePost({ id: postId, updatedPost: updatedPostData }));
+  }
+
+  const handleDeletePost = (postId) => {
+    dispatch(deletePost(postId));
+  }
+
+  const handleLikePost = (postId) => {
+    dispatch(likePost(postId));
+  }
+
     const classes = useStyles();
     return (
         <>
